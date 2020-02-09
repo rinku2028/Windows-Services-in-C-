7c845823805b0939766ba096ff6c1fedab2b7950
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 01:32:59 +0545

    Service start, stop and restart from C#

---
+++ Windowsservice.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Windowsservice.aspx.cs" Inherits="Windows_Service.Windowsservice" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnstart" Text="Start" runat="server" OnClick="btnstart_Click" />
+            <asp:Label ID="lblmessage" Text="" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Windowsservice.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Windows_Service
+{
+    public partial class Windowsservice : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void StartService(string serviceName, int timeoutMilliseconds)
+        {
+            ServiceController service = new ServiceController(serviceName);
+            try
+            {
+                TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+                
+            }
+            catch
+            {
+                // ...
+            }
+        }
+        protected void btnstart_Click(object sender, EventArgs e)
+        {
+            string ss = "iis";
+            StartService(ss,10);
+            lblmessage.Text = "service start";
+
+        }
+    }
+}
